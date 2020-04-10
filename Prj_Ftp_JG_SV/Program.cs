cf54d89a0b13637bc60a83f66a9e8dc6ed3e4a79
sebastian.villada <jsebastian1422@gmail.com>
2020-04-09 22:30:46 -0500

    Cosas nuevas

--- Prj_Ftp_JG_SV/Program.cs
+++ Prj_Ftp_JG_SV/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace Prj_Ftp_JG_SV
     {
         static void Main(string[] args)
         {
+            DirectoryInfo di = new DirectoryInfo(@"..\..\Documentos");
+            Console.WriteLine("No search pattern returns:");
+            Console.WriteLine(di.Name + "\n");
+            foreach (var fi in di.GetFiles())
+            {
+                string[] lineas = File.ReadAllLines(di + "/" + fi.Name);
+                foreach (var item in lineas)
+                {
+                    var valores = item.Split(';');
+                    Console.Write(valores[0]);
+                }
+
+                //Console.WriteLine(fi.Name + "\n");
+            }
+       
+            Console.WriteLine();
         }
     }
 }
